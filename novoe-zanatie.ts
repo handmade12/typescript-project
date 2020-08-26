a8c8d889b40a282c1ed6a851d6fd705e3aa5dfbc
Kirill <Kirilldibil02@gmail.com>
2020-08-26 18:59:04 +0300

    Занятие прошло успешно!

---
+++ novoe-zanatie.ts
@@ -0,0 +1,29 @@
+/*
+function add(a: number, b: number) {
+  return a + b;
+}
+function sub(a: number, b: number) {
+  return a - b;
+}
+function mult(a: number, b: number) {
+  return a * b;
+}
+function del(a: number, b: number) {
+  if (b == 0) {
+    return "нельзя делить на ноль!";
+  } else {
+    return a / b;
+  }
+}
+var x = prompt("Введите первое число");
+var y = prompt("Введите второе число");
+
+if (x == null || y == null) {
+  console.log("чтото не ввели");
+} else {
+  alert(del(+x, +y));
+}
+*/
+function clicktb() {
+  alert("hello!");
+}
